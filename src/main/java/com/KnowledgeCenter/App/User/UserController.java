05efa57325b36153930de9b091ce85e9629bb682
Diego Torres <diegotm.90@gmail.com>
2021-11-07 16:54:26 +0100

    Added password encription

--- src/main/java/com/KnowledgeCenter/App/User/UserController.java
+++ src/main/java/com/KnowledgeCenter/App/User/UserController.java
@@ -1,17 +1,22 @@
 package com.KnowledgeCenter.App.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 	
 	@Autowired
 	UserService userService;
 	
 	@PostMapping("users")
+	@ResponseStatus(HttpStatus.CREATED)
 	void addUser(@RequestBody User user) {
 			userService.save(user);
 	}
--- src/main/java/com/KnowledgeCenter/App/User/UserService.java
+++ src/main/java/com/KnowledgeCenter/App/User/UserService.java
@@ -1,19 +1,23 @@
 package com.KnowledgeCenter.App.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 		
 	UserRepository userRepository;
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public UserService(@Autowired UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 	
 	public User save( User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		return userRepository.save(user);
 	}
 

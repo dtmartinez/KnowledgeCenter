9d3dd43f4968d0c91355e0e1a7e98fff71aef065
Diego Torres <diegotm.90@gmail.com>
2021-11-08 12:13:59 +0100

    Added a sign up form

--- webapp/src/pages/UserSignUpPage.js
+++ webapp/src/pages/UserSignUpPage.js
@@ -4,6 +4,21 @@ export class UserSignUpPage extends React.Component {
 	render(){
 		return(<div>
 			<h1>Sign Up</h1>
+			<div>
+				<label for = "Name">Name</label>
+				<input id = "Name"></input>
+			</div>
+			<div>
+				<label for = "Password">Password</label>
+				<input id = "Password" type = "password"></input>
+			</div>
+			<div>
+				<label for = "ConfirmPassword">ConfirmPassword</label>
+				<input id = "ConfirmPassword" type = "password"></input>
+			</div>	
+			<div>
+				<button name = 'submit'>Sign up</button>
+			</div>		
 		</div>);
 	}
 	
--- webapp/src/pages/UserSignUpPage.test.js
+++ webapp/src/pages/UserSignUpPage.test.js
@@ -7,11 +7,52 @@ describe('UserSignUpPage', () => {
 	
 	describe('Layout', () => {
 		
-		it('has header of Sign up ',() => {
+		it('has a Sign up header',() => {
 			const { container } = render(<UserSignUpPage />);
 			const header = container.querySelector('h1');
 			expect(header).toHaveTextContent('Sign Up');
 		})
+
+		it('has input form for Name', () =>  {
+			const { getByLabelText  } = render(<UserSignUpPage />);
+			const nameInput = getByLabelText('Name');
+			expect(nameInput).toBeInTheDocument;
+		})
+
+		it('has input form for password', () =>  {
+			const { getByLabelText  } = render(<UserSignUpPage />);
+			const passwordInput = getByLabelText('Password');
+			expect(passwordInput).toBeInTheDocument;
+		})
+
+		it('has input form for confirm password', () =>  {
+			const { getByLabelText  } = render(<UserSignUpPage />);
+			const ConfirmPasswordInput = getByLabelText('ConfirmPassword');
+			expect(ConfirmPasswordInput).toBeInTheDocument;
+		})
+
+		it('has password type in password input forms', () =>  {
+			const { getByLabelText  } = render(<UserSignUpPage />);
+			const passwordInput = getByLabelText('Password');
+			expect(passwordInput.type).toBe('password');			
+		})
+
+		it('has password type in confirm password input forms', () =>  {
+			const { getByLabelText  } = render(<UserSignUpPage />);
+			const ConfirmPasswordInput = getByLabelText('Password');
+			expect(ConfirmPasswordInput.type).toBe('password');			
+		})
+
+		it('has a submit button ', () =>  {
+			const { getByRole  } = render(<UserSignUpPage />);
+			const submitButton = getByRole('button', { name : /Sign up/i});
+			expect(submitButton).toBeInTheDocument;
+		})
+
+
+
+
+		
 	
 	})
 

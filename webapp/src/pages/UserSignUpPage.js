d4a0bd4ce08747332df0e00deca0fbd5e294030b
Diego Torres <diegotm.90@gmail.com>
2021-11-11 10:27:20 +0100

    Added UserSignup event handlers

--- webapp/src/pages/UserSignUpPage.js
+++ webapp/src/pages/UserSignUpPage.js
@@ -1,28 +1,78 @@
 import React from 'react';
 
 export class UserSignUpPage extends React.Component {
+
+	state = {
+		name : '',
+		password : '',
+		confirmPassword : ''
+	};
+
+	handleNameChange = (event) => {		
+		const value = event.target.value;
+		this.setState({name : value });
+	}
+	handlePasswordChange = (event) => {		
+		const value = event.target.value;
+		this.setState({password : value });
+	}
+	handleConfirmPasswordChange = (event) => {		
+		const value = event.target.value;
+		this.setState({confirmPassword : value });
+	}
+
+	handleOnClickSignUp = (event) => {		
+		this.props.actions.postSignUp();
+	}
+
 	render(){
 		return(<div>
 			<h1>Sign Up</h1>
 			<div>
-				<label for = "Name">Name</label>
-				<input id = "Name"></input>
+				<label>
+					Name
+					<input id = "Name"  
+					value={this.state.name}  
+					onChange={this.handleNameChange}></input>
+				</label>				
 			</div>
 			<div>
-				<label for = "Password">Password</label>
-				<input id = "Password" type = "password"></input>
+				<label>
+					Password
+					<input id = "Password"
+					value={this.state.password} 
+					onChange={this.handlePasswordChange}
+					type = "password"></input>
+				</label>
+				
 			</div>
 			<div>
-				<label for = "ConfirmPassword">ConfirmPassword</label>
-				<input id = "ConfirmPassword" type = "password"></input>
+				<label>
+					ConfirmPassword
+					<input id = "ConfirmPassword"
+					value={this.state.confirmPassword} 
+					onChange={this.handleConfirmPasswordChange}
+					type = "password"></input>
+				</label>				
 			</div>	
 			<div>
-				<button name = 'submit'>Sign up</button>
+				<button 
+					onClick= {this.handleOnClickSignUp}>
+					Sign up
+				</button>
 			</div>		
 		</div>);
 	}
 	
 	
+}
+
+UserSignUpPage.defaultProps = {
+	actions : {
+		postSignUp : () => new Promise((resolve, reject) => {
+			resolve({});
+		})
+	}
 }
 
 export default UserSignUpPage;

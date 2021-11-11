6cec4b455a7bfa52d3db69f815c87902ea6ecbbc
Diego Torres <diegotm.90@gmail.com>
2021-11-11 10:27:20 +0100

    Added UserSignup event handlers

--- webapp/src/pages/UserSignUpPage.test.js
+++ webapp/src/pages/UserSignUpPage.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { UserSignUpPage	} from './UserSignUpPage';
+import userEvent from '@testing-library/user-event';
 
 describe('UserSignUpPage', () => {
 	
-	describe('Layout', () => {
+	describe('Rendering', () => {
 		
 		it('has a Sign up header',() => {
 			const { container } = render(<UserSignUpPage />);
@@ -47,13 +48,56 @@ describe('UserSignUpPage', () => {
 			const { getByRole  } = render(<UserSignUpPage />);
 			const submitButton = getByRole('button', { name : /Sign up/i});
 			expect(submitButton).toBeInTheDocument;
+		})	
+	})
+
+	describe('Events', () => {
+		it('sets the name value', () => {
+			const { getByLabelText  } = render(<UserSignUpPage />);
+			const nameInput = getByLabelText('Name');
+			userEvent.type(nameInput, "username");
+			expect(nameInput).toHaveValue("username");
 		})
 
+		it('sets the password value', () =>  {
+			const { getByLabelText  } = render(<UserSignUpPage />);
+			const passwordInput = getByLabelText('Password');
+			userEvent.type(passwordInput, "P4sword1");
+			expect(passwordInput).toHaveValue("P4sword1");
+		})
 
+		it('sets the confirmpassword value', () =>  {
+			const { getByLabelText  } = render(<UserSignUpPage />);
+			const ConfirmPasswordInput = getByLabelText('ConfirmPassword');
+			userEvent.type(ConfirmPasswordInput, "P4sword1");
+			expect(ConfirmPasswordInput).toHaveValue("P4sword1");
+		})
 
-
+		it('creates post request when fields are valid ', () =>  {
+			
+			const actions = {
+				postSignUp: jest.fn().mockResolvedValueOnce({})
+			};
+			
+			const { getByLabelText , getByRole } =
+				 render(<UserSignUpPage actions={actions}/>);	
+					
+			const nameInput = getByLabelText('Name');
+			const passwordInput = getByLabelText('Password');
+			const confirmPasswordInput = getByLabelText('ConfirmPassword');
+			const submitButton = getByRole('button', { name : /Sign up/i});
 		
+			userEvent.type(nameInput, "username");
+			userEvent.type(passwordInput, "P4sword1");
+			userEvent.type(confirmPasswordInput, "P4sword1");
+			userEvent.click(submitButton);		
+		
+			expect(actions.postSignUp).toHaveBeenCalledTimes(1);
+		})
+
 	
+		
+
 	})
 
 

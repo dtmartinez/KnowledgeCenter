c30aed8d0c52e92e4aa6ddd8458a8021e6705926
Diego Torres <diegotm.90@gmail.com>
2021-11-13 12:28:32 +0100

    Added user validation

--- src/main/java/com/KnowledgeCenter/App/User/User.java
+++ src/main/java/com/KnowledgeCenter/App/User/User.java
@@ -3,6 +3,9 @@ package com.KnowledgeCenter.App.User;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -12,8 +15,13 @@ public class User {
 	
 	@Id
 	@GeneratedValue
-	private long id;
 	
+	private long id;
+	@NotBlank(message = "Name can not be empty")
+	@Size(min = 4, max = 100 , message = "Name size must be between 4 and 15")
 	private String name;
+	@NotBlank(message = "Password can not be empty")
+	@Size(min = 8, message = "Name size must be between 4 and 15")
+	@Pattern(regexp ="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$" , message = "Password must contain one lowercase upercase and number")
 	private String password;
 }
